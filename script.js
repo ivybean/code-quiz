2e90ca2ac34347f3509917e91edd98e1a78b3b48
Ivy Bean <ivieking@gmail.com>
2020-07-20 12:59:14 -0400

    Updated Styles

---
+++ script.js
@@ -0,0 +1,15 @@
+var startButton = document.getElementById("start-btn")
+
+startButton.addEventListener('click', startGame)
+
+function startGame(){
+  console.log("It has begun!");
+}
+
+function setNextQuestion(){
+
+}
+
+function answerQuestion(){
+
+}
